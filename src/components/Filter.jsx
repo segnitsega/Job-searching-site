d8180f2fe1d29b8ce3573bf345ffda97d17aee1e
Segni <segnitsega6@gmail.com>
2025-02-09 00:14:31 -0800

    modify Filter.jsx

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,13 +1,16 @@
 
 import { useState } from "react";
 import { IoIosArrowDown, IoIosArrowUp   } from "react-icons/io";
+import { FaMapMarkerAlt } from "react-icons/fa";
 
 const Filter = () => {
     const [selectedValue, setSelectedValue] = useState("Last 24 Hours");
     const options = ["Last Hour", "Last Week", "Last Month", "Last Year"];
-
     const [isopen, setIsopen] = useState(false)
 
+    const [level, setLevel] = useState("Intermediate")
+    const levels = ["Junior","Intermediate","Senior"]
+    const [levelOpen, setIslevelOpen] = useState(false)
 
   return (
     <div className="flex flex-col w-[350px] shadow-lg shadow-gray-300 border border-gray-200  rounded-2xl ">
@@ -61,6 +64,48 @@ const Filter = () => {
                 </div>                        
                
             </div>
+
+                <div className="flex flex-col">
+                        <label>Location</label>
+                        <div className="flex items-center gap-1 border border-gray-300 p-2 rounded-md mb-2"> 
+                            <FaMapMarkerAlt className="text-gray-500 " />
+                            <input type="text" placeholder="Enter your location" className="outline-none"/>
+                        </div>
+                            
+                        <div className="mb-4 ">
+                        <p>Experience Level</p>
+                        <input 
+                            type="text"
+                            value= {level}
+                            readOnly
+                            className="relative w-full border px-3 py-2 rounded-md focus:outline-none"
+                            onClick={()=>setIslevelOpen(!levelOpen)}
+
+                        />
+                        <button className="absolute left-65 top-1/2 -translate-y-5" tabIndex={-1} onClick={()=>setIslevelOpen(!levelOpen)}>
+                        {/* {
+                            levelOpen ? <IoIosArrowUp /> : <IoIosArrowDown  />
+                        } */}
+                            
+                        </button>
+                        {
+                            levelOpen && (
+                                <ul className=" w-full bg-white border rounded-md mt-1">
+                                {levels.map((level, index) => (
+                                <li
+                                    key={index}
+                                    className="p-2 hover:bg-gray-200 cursor-pointer"
+                                    onClick={() => {setLevel(level); setIslevelOpen(false)}}
+                                >
+                                    {level}
+                                </li>
+                            ))}
+                        </ul>
+                            )
+                        }
+                    </div>
+                
+                </div>
             
         </div>
                    
