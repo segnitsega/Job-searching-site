9965e4a1ef289e2abe654eae4a8226e1d139ef59
Segni <segnitsega6@gmail.com>
2025-02-12 04:04:25 -0800

    style SavedJobs

--- src/components/SavedJobs.jsx
+++ src/components/SavedJobs.jsx
@@ -12,24 +12,28 @@ function SavedJobs({jobsList, setjobsList}) {
     )
     
   return (
-    <div className="flex flex-col items-center w-[330px] shadow-lg shadow-gray-300 border border-gray-200  rounded-2xl ">
+    <div className="inline-flex flex-col h-fit items-center w-[270px] shadow-lg shadow-gray-300 border border-gray-200  rounded-2xl ">
       <h1 className="text-[32px]">Saved Jobs</h1>
      
         {
                 jobsList.map((job, index)=>(
                     job.isBookMarked && 
-                    <div key={index} className='flex flex-col items-center w-[280px] border border-gray-200 shadow-lg shadow-gray-300 rounded-2xl p-4 mb-4'> 
-                    <div  className="flex gap-20 p-2 ">  
-                        <div >
-                            <h2>{job.type}</h2>
-                            {/* <p>{job.model}</p> */}
-                            <p>{job.salary}</p>  
-                        </div>
-                        <div className="">
-                            <IoCloseOutline size={20} onClick={() => handleClick(job.id)}/>  
-                        </div>
-                    </div> 
-                     </div>
+                    <div key={index} className='flex flex-col w-[230px] border border-gray-200 shadow-lg shadow-gray-300 rounded-2xl p-2 mb-4'> 
+                      <div  className="flex justify-between p-1 ">  
+                          <div className="ml -4">
+                            <h1 className="text-[20px]">{job.title}</h1>
+                            <p className="text-sm">{job.company}</p>
+                            <div className="flex gap-2 text-sm">
+                              <h2 className="bg-gray-200 p-1">{job.type}</h2>                              
+                              <p className="bg-gray-200 p-1">{job.salary}</p>
+                            </div>
+  
+                          </div>
+                          <div className="">
+                              <IoCloseOutline size={20} onClick={() => handleClick(job.id)}/>  
+                          </div>
+                      </div> 
+                    </div>
                 ))
             }
         
